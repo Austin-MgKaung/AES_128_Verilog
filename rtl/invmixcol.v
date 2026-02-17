b5c5a829a411ae59a9ed69afd4e2471d1267462d
kramachandran1-tech <kramachandran1@sheffield.ac.uk>
2026-02-17 16:18:17 +0000

    Update invmixcol.v

--- rtl/invmixcol.v
+++ rtl/invmixcol.v
@@ -1,111 +1,94 @@
 `timescale 1ns/1ps
 
-module invmixcol  (
-    input  wire           clk,       
-    input  wire [31:0]    col_in,
-    output wire [31:0]    col_out
+module invmixcol (
+    input  wire        clk,
+    input  wire [31:0] col_in,
+    output wire [31:0] col_out
 );
 
-wire [7:0] a = col_in[31:24];
-wire [7:0] b = col_in[23:16];
-wire [7:0] c = col_in[15:8];
-wire [7:0] d = col_in[7:0];
+    wire [7:0] a = col_in[31:24];
+    wire [7:0] b = col_in[23:16];
+    wire [7:0] c = col_in[15:8];
+    wire [7:0] d = col_in[7:0];
 
-function [7:0] xtime;
-    input [7:0] x;
-    begin
-        xtime = {x[6:0],1'b0} ^ (8'h1B & {8{x[7]}});
+    function automatic [7:0] xtime(input [7:0] x);
+        begin
+            xtime = {x[6:0],1'b0} ^ (8'h1B & {8{x[7]}});
+        end
+    endfunction
+
+
+    reg [7:0] a2, a4, a8;
+    reg [7:0] b2, b4, b8;
+    reg [7:0] c2, c4, c8;
+    reg [7:0] d2, d4, d8;
+
+    wire [7:0] a2_n = xtime(a);
+    wire [7:0] a4_n = xtime(a2_n);
+    wire [7:0] a8_n = xtime(a4_n);
+
+    wire [7:0] b2_n = xtime(b);
+    wire [7:0] b4_n = xtime(b2_n);
+    wire [7:0] b8_n = xtime(b4_n);
+
+    wire [7:0] c2_n = xtime(c);
+    wire [7:0] c4_n = xtime(c2_n);
+    wire [7:0] c8_n = xtime(c4_n);
+
+    wire [7:0] d2_n = xtime(d);
+    wire [7:0] d4_n = xtime(d2_n);
+    wire [7:0] d8_n = xtime(d4_n);
+
+
+    reg [7:0] a_s1, b_s1, c_s1, d_s1;
+
+    always @(posedge clk) begin
+        a2 <= a2_n; a4 <= a4_n; a8 <= a8_n;
+        b2 <= b2_n; b4 <= b4_n; b8 <= b8_n;
+        c2 <= c2_n; c4 <= c4_n; c8 <= c8_n;
+        d2 <= d2_n; d4 <= d4_n; d8 <= d8_n;
+
+        a_s1 <= a; b_s1 <= b; c_s1 <= c; d_s1 <= d;
     end
-endfunction
-
-//Start pipelining
-
-reg [7:0] a2, a4, a8, b2, b4, b8, c2, c4, c8, d2, d4, d8;
-
-always @(posedge clk) begin 
-
-    a2 <= xtime(a);
-    a4 <= xtime(a2);
-    a8 <= xtime(a4);
-
-    b2 <= xtime(b);
-    b4 <= xtime(b2);
-    b8 <= xtime(b4);
-
-    c2 <= xtime(c);
-    c4 <= xtime(c2);
-    c8 <= xtime(c4);
-
-    d2 <= xtime(d);
-    d4 <= xtime(d2);
-    d8 <= xtime(d4);
-
-end
-
-reg [7:0] a_e_init, a_e, b_bi_init, b_bi, c_di_init, c_di, d_9, a_9, b_e_init, b_e, c_bi, c_bi_init, d_di, d_di_init, a_di, a_di_init, b_9, c_e, c_e_init, d_bi, d_bi_init, a_bi, a_bi_init, b_di, b_di_init, c_9, d_e_init, d_e;
-
-always @(posedge clk) begin
-
-    a_e_init  <= a8 ^ a4;
-    a_e  <= a_e_init ^ a2;
-    b_bi_init <= b8 ^ b2;
-    b_bi <= b_bi_init ^ b;
-    c_di_init <= c8 ^ c4;
-    c_di <= c_di_init ^ c;
-    d_9  <= d8 ^ d;
-
-    a_9  <= a8 ^ a;
-    b_e_init  <= b8 ^ b4;
-    b_e  <= b_e_init ^ b2;
-    c_bi_init <= c8 ^ c2;
-    c_bi <= c_bi_init ^ c;
-    d_di_init <= d8 ^ d4;
-    d_di <= d_di_init ^ d;
 
 
-    a_di_init <= a8 ^ a4;
-    a_di <= a_di_init ^ a;
-    b_9  <= b8 ^ b;
-    c_e_init  <= c8 ^ c4;
-    c_e  <= c_e_init ^ c2;
-    d_bi_init <= d8 ^ d2;
-    d_bi <= d_bi_init ^ d;
+    reg [7:0] aE, aB, aD, a9;
+    reg [7:0] bE, bB, bD, b9;
+    reg [7:0] cE, cB, cD, c9;
+    reg [7:0] dE, dB, dD, d9;
 
-    a_bi_init <= a8 ^ a2;
-    a_bi <= a_bi_init ^ a;
-    b_di_init <= b8 ^ b4;
-    b_di <= b_di_init ^ b;
-    c_9  <= c8 ^ c;
-    d_e_init  <= d8 ^ d4;
-    d_e  <= d_e_init ^ d2;
+    always @(posedge clk) begin
+        // a*
+        aE <= a8 ^ a4 ^ a2;      // 0x0E
+        aB <= a8 ^ a2 ^ a_s1;    // 0x0B
+        aD <= a8 ^ a4 ^ a_s1;    // 0x0D
+        a9 <= a8 ^ a_s1;         // 0x09
 
-end
+        bE <= b8 ^ b4 ^ b2;
+        bB <= b8 ^ b2 ^ b_s1;
+        bD <= b8 ^ b4 ^ b_s1;
+        b9 <= b8 ^ b_s1;
 
-reg [7:0] out0_comb, out0_comb_a, out0_comb_b, out1_comb, out1_comb_a, out1_comb_b, out2_comb_a, out2_comb_b, out2_comb, out3_comb_a, out3_comb_b, out3_comb;
+        cE <= c8 ^ c4 ^ c2;
+        cB <= c8 ^ c2 ^ c_s1;
+        cD <= c8 ^ c4 ^ c_s1;
+        c9 <= c8 ^ c_s1;
 
-always @(posedge clk) begin
+        dE <= d8 ^ d4 ^ d2;
+        dB <= d8 ^ d2 ^ d_s1;
+        dD <= d8 ^ d4 ^ d_s1;
+        d9 <= d8 ^ d_s1;
+    end
 
-    out0_comb_a <= a_e  ^ b_bi;  
-    out0_comb_b <= c_di ^ d_9;
-    out0_comb <= out0_comb_a ^ out0_comb_b;
+    reg [7:0] o0, o1, o2, o3;
 
-    out1_comb_a <= a_9  ^ b_e;
-    out1_comb_b <= c_bi ^ d_di;
-    out1_comb <= out1_comb_a   ^ out1_comb_b;
-    
-    out2_comb_a <= a_di ^ b_9; 
-    out2_comb_b <= c_e  ^ d_bi;
-    out2_comb <= out2_comb_a   ^ out2_comb_b;
-    
-    out3_comb_a <= a_bi ^ b_di;  
-    out3_comb_b <= c_9  ^ d_e;
-    out3_comb <= out3_comb_a  ^ out3_comb_b;
+    always @(posedge clk) begin
+        o0 <= aE ^ bB ^ cD ^ d9;
+        o1 <= a9 ^ bE ^ cB ^ dD;
+        o2 <= aD ^ b9 ^ cE ^ dB;
+        o3 <= aB ^ bD ^ c9 ^ dE;
+    end
 
-end
-
-assign col_out = {out0_comb, out1_comb, out2_comb, out3_comb};
+    assign col_out = {o0, o1, o2, o3};
 
 endmodule
-
-
-
